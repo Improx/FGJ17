395eab13b7bcc2b8f2033d874a17d31331493269
wawethewaras <wawe212@hotmail.fi>
2017-01-21 00:59:27 +0200

    Football and goals

---
+++ Assets/Scripts/AI/GoalController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 ballPosition;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Football") {
+            print("Score!!");
+            other.transform.position = ballPosition;
+        }
+    }
+}
