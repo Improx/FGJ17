1bd41b3ea4914e11a1513b3088b4fde563eb78f4
Jussi Joki <judejoki@gmail.com>
2017-01-20 23:17:36 +0200

    More wave controll

--- Assets/Scripts/Stadium.cs
+++ Assets/Scripts/Stadium.cs
@@ -5,46 +5,75 @@ using UnityEngine;
 public class Stadium : MonoBehaviour {
 
     public List<ListWrapper> Columns = new List<ListWrapper>();
+    
 
-    public bool DoWave;
-    public float wavePos;
-    public float waveSpeed;
-    public float waveLength;
+    public List<Wave> Waves;
 
     public float maxYOffset;
 
     public int rowLength;
 
+    private static Stadium instance;
+    public static Stadium Instance {
+        get {
+            if (!instance) {
+                instance = GameObject.FindObjectOfType<Stadium>();
+            }
+
+            return instance;
+        }
+    }
+
     // Use this for initialization
     void Start () {
-        wavePos = -waveLength;
-	}
+        Waves = new List<Wave>();
+        GenerateRandomWave();
+        //GenerateRandomWaveReverse();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (DoWave) {
-            rowLength = Columns.Count;
+        rowLength = Columns.Count;
 
-            wavePos = Time.deltaTime * waveSpeed + wavePos;
-            if (wavePos > (rowLength + waveLength)) {
-                wavePos = -waveLength;
+        var maxes = new float[rowLength];
+        foreach (var wave in Waves) {
 
+            wave.Tick(Time.deltaTime);
 
+            if (wave.IsDone) {
+                Waves.Remove(wave);
+                continue;
             }
+
 
             for (int i = 0; i < Columns.Count; i++) {
                 foreach (var seats in Columns[i].Seats) {
-                    float offset = 0;
-                    if ((i - waveLength) < wavePos && (i + waveLength) > wavePos) {
-                        offset = (waveLength - Mathf.Abs(wavePos - i)) / waveLength * maxYOffset;
-                    }
-                    if (seats.Ocupant) {
-                        seats.Ocupant.SetYOffset(offset);
+                    if ((i - wave.waveLength) < wave.wavePos && (i + wave.waveLength) > wave.wavePos) {
+                        maxes[i] = Mathf.Max((wave.waveLength - Mathf.Abs(wave.wavePos - i)) / wave.waveLength * maxYOffset, maxes[i]);
                     }
                 }
             }
         }
-	}
+
+        for (int i = 0; i < Columns.Count; i++) {
+            foreach (var seat in Columns[i].Seats) {
+                if (seat.Ocupant) {
+                    seat.Ocupant.SetYOffset(maxes[i]);
+                }
+            }
+        }
+
+    }
+
+    public void GenerateRandomWave() {
+        var wave = new Wave(2, 1.5f, false);
+        Waves.Add(wave);
+    }
+
+    public void GenerateRandomWaveReverse() {
+        var wave = new Wave( 2, 1.5f, true);
+        Waves.Add(wave);
+    }
 }
 
 [System.Serializable]
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,39 @@
+﻿
+[System.Serializable]
+public class Wave {
+    public float wavePos;
+    public float waveSpeed;
+    public float waveLength;
+    public bool reverse;
+    
+    public Wave(float speed, float length, bool rev) {
+        waveLength = length;
+        waveSpeed = speed;
+        reverse = rev;
+        if (reverse) {
+            wavePos = Stadium.Instance.rowLength + waveLength;
+        } else {
+            wavePos = -waveLength;
+        }
+    }
+
+    public Wave(float pos, float speed, float length) {
+        waveLength = length;
+        waveSpeed = speed;
+        wavePos = pos;
+    }
+
+    public bool IsDone {
+        get {
+            if (reverse) {
+                return wavePos <= -waveLength;
+            } else {
+                return wavePos >= (Stadium.Instance.rowLength + waveLength);
+            }
+        }
+    }
+
+    public void Tick (float delta) {
+        wavePos = delta * waveSpeed + wavePos;
+    }
+}
