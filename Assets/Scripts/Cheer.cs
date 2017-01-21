c87362c4a76491cf4f40a8cbde7f27fd25a44b23
Lauri Lappalainen <lauri.s.lappalainen@gmail.com>
2017-01-21 15:56:26 +0200

    Standing animation 1.0

--- Assets/Scripts/Cheer.cs
+++ Assets/Scripts/Cheer.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cheer : MonoBehaviour {
-	[Range(0.0f, 100.0f)]
+	[Range(0.0f, 1.0f)]
 	[SerializeField] private float frame;
 	Animation anim;
+	public AnimationClip cheer;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animation> ();
-		anim ["Excited"].clip.legacy = true;
+		cheer.legacy = true;
+		anim.AddClip (cheer, "Excited");
+		anim ["Excited"].time = 1.0f;
+		anim ["Excited"].speed = 0.0f;
+		anim.Play ("Excited");
 		/*anim.Play ("Excited");
 		anim ["Excited"].speed = 0;*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//anim ["Excited"].time = frame;
+		anim ["Excited"].time = frame;
 	}
 }
