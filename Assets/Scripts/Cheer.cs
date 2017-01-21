0534f665ecfdc8b00f87c13f2d2b2cf22a037e18
Lauri Lappalainen <lauri.s.lappalainen@gmail.com>
2017-01-21 15:32:06 +0200

    Animaatiotyömää

---
+++ Assets/Scripts/Cheer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheer : MonoBehaviour {
+	[Range(0.0f, 100.0f)]
+	[SerializeField] private float frame;
+	Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+		anim ["Excited"].clip.legacy = true;
+		/*anim.Play ("Excited");
+		anim ["Excited"].speed = 0;*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//anim ["Excited"].time = frame;
+	}
+}
