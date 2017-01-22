ebe3ddd0711c1f746e80a43fc013fe2c97f609e4
Miika <miika.kanerva@aalto.fi>
2017-01-22 11:32:09 +0200

    pausemenu fixed & scoreboard and goal connection thru script

--- Assets/Scripts/AI/GoalController.cs
+++ Assets/Scripts/AI/GoalController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalController : MonoBehaviour {
 
@@ -8,6 +9,16 @@ public class GoalController : MonoBehaviour {
     private Vector3 ballPosition;
 	[SerializeField]
 	private static Stadium instance;
+    [SerializeField]
+	public Text ScoreTextObject;
+
+    public int score = 0;
+
+    void Start()
+    {
+        ScoreTextObject.text = score.ToString();
+    }
+
 	public static Stadium Instance {
 		get {
 			if (!instance) {
@@ -20,9 +31,12 @@ public class GoalController : MonoBehaviour {
 
     void OnTriggerEnter(Collider other) {
         if (other.tag == "Football") {
-            //print("Score!!");
+            print("Score!!");
 			Instance.GenerateRandomWave ();
             other.transform.position = ballPosition;
+            score += 1;
+            ScoreTextObject.text = score.ToString();
+            print(score);
         }
     }
 }
